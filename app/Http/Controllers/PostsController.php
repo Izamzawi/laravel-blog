a89a80ce33f4e49f9dd819b377eaa3c4d827c8ba
Zamzawi <idhamzamzawi@gmail.com>
2021-06-19 17:51:48 +0700

    Remove unused dependencies

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Cviebrock\EloquentSluggable\Services\SlugService;
 use App\Models\Post;
 
 class PostsController extends Controller
